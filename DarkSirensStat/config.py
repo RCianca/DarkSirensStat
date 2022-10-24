8ecf9c2c8d3a28dd44ec5a51da6ec3d8aee4e6cd
RCianca <rciancarella@os.uniroma3.it>
2022-10-24 10:32:14 +0200

    commit before line of sight test

--- DarkSirensStat/config.py
+++ DarkSirensStat/config.py
@@ -15,7 +15,7 @@ goalParam = 'H0'
 
 ## Output folder name
 #fout = 'ET_newsigma'
-fout = 'ET_2GW_rate_500gal2host'
+fout = 'ET_6GW_rate_500gal6host'
 
 #only to some  test, then remove 
 delta=1#(0.318639,0.674490,0.977925,0.994458,1.281552,1.644854,1.959964,2)
@@ -81,8 +81,8 @@ zLimSelection='skymap'
 #subset_names = ['GW190924_021846','GW190527_092055','GW190814','GW190708_232457','GW190412','GW190421_213856','GW190708_232457','GW190915_235702']
 #subset_names = ['GW220803','GW220804','GW220805','GW220806','GW220807','GW220808','GW220809']
 #subset_names = ['GW220810']
-#subset_names = ['GW221003','GW221004','GW221005','GW221006','GW221007','GW221008_01']
-subset_names = ['GW221005','GW221007']
+subset_names = ['GW221003','GW221004','GW221005','GW221006','GW221007','GW221009']
+#subset_names = ['GW221005','GW221007','GW221006']
 #subset_names =  None
 
 ## Select events based on completeness at the nominal position
