c9fa5e919f87bdad28d6ded5750eb403bea34b67
RCianca <rciancarella@os.uniroma3.it>
2022-11-11 10:39:29 +0100

    merged ET_class

--- DarkSirensStat/config.py
+++ DarkSirensStat/config.py
@@ -15,7 +15,7 @@ goalParam = 'H0'
 
 ## Output folder name
 #fout = 'ET_newsigma'
-fout = 'ET_coneincrement001k'
+fout = 'ET'
 
 #only to some  test, then remove 
 delta=1#(0.318639,0.674490,0.977925,0.994458,1.281552,1.644854,1.959964,2)
